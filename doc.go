254c0da085e38c28125a803e51c6232d8476bd0f
youwkey <youwkey@gmail.com>
2021-04-30 01:23:12 +0900

    docs: Add Copylight

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Copyright 2021 youwkey. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package alfred
--- icons.go
+++ icons.go
@@ -1,3 +1,7 @@
+// Copyright 2021 youwkey. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
 package alfred
 
 const systemIconDirPath = "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/"
